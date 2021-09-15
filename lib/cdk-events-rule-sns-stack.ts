c6d905786fa4e6d7acc84ea9164112b976af1892
EC2 Default User <ec2-user@ip-172-31-29-201.us-east-2.compute.internal>
2021-09-15 15:34:26 +0000

    first commit

--- lib/cdk-events-rule-sns-stack.ts
+++ lib/cdk-events-rule-sns-stack.ts
@@ -1,9 +1,36 @@
 import * as cdk from '@aws-cdk/core';
 
+import * as sns from "@aws-cdk/aws-sns";
+import * as events from "@aws-cdk/aws-events";
+import * as targets from "@aws-cdk/aws-events-targets";
+
+
 export class CdkEventsRuleSnsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    
+    const topic = new sns.Topic(this, "topic");
+    
+    const rule = new events.Rule(this, "testRule", {
+      schedule: events.Schedule.expression('rate(1 minute)')
+    });
+    
+    rule.addTarget(
+      new targets.SnsTopic(topic, {
+          message: events.RuleTargetInput.fromObject(
+            {
+              DataType: `This_is_a_${events.EventField.fromPath('$.detail-type')}`,
+              RuleName: `<aws.events.rule-name>`,
+              RuleArn: `<aws.events.rule-arn>`,
+              Time: `<aws.events.event.ingestion-time>`
+            }
+          )
+      })
+    );
+    
+    
+    
   }
 }
